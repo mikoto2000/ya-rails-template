371d7c59881bef6c6ed948d5b842beb17bf85a5e
mikoto2000 <mikoto2000@gmail.com>
2023-05-30 11:53:53 +0000

    Fixed lint issues
    テストコードの lint 対応は保留

--- templates/active_record/model/model.rb
+++ templates/active_record/model/model.rb
@@ -1,20 +1,18 @@
 class <%= plural_table_name.classify %> < ApplicationRecord
-  def self.ransackable_attributes(auth_object = nil)
-    <%=
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[<%=
       attributes.map { |e|
         if e.reference?
           e.name + '_id'
         else
           e.name
         end
-      }.concat(["id", "created_at", "updated_at"]).to_s
-    %>
+      }.concat(["id", "created_at", "updated_at"]).join(" ")
+    %>]
   end
-
 <% attributes.reject(&:password_digest?).each do |attribute| -%>
 <% if attribute.reference? -%>
   <%= attribute.type %> :<%= attribute.name %>
 <% end -%>
 <% end -%>
 end
-
